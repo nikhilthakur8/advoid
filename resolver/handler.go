18e4277a6776890b122b43dd6085319d8c29dc51
Nikhil Thakur <nikhilthakur8012004@gmail.com>
2025-10-30 03:36:52 +0530

    Add domain blocking functionality to HandleDNSRequest

--- resolver/handler.go
+++ resolver/handler.go
@@ -4,12 +4,37 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"strings"
 
 	"github.com/miekg/dns"
 	"github.com/nikhilthakur8/advoid/upstreams"
 )
 
+var blockedDomains = map[string]bool{
+	"ad.doubleclick.net":             true,
+	"ads.youtube.com":                true,
+	"googleads.g.doubleclick.net":    true,
+	"pagead.l.doubleclick.net":       true,
+	"pubads.g.doubleclick.net":       true,
+	"partnerad.l.doubleclick.net":    true,
+	"adservice.google.com":           true,
+	"adservice.google.co.in":         true,
+	"www.googleadservices.com":       true,
+	"gads.g.doubleclick.net":         true,
+	"securepubads.g.doubleclick.net": true,
+	"static.doubleclick.net":         true,
+}
+
 func HandleDNSRequest(w dns.ResponseWriter, r *dns.Msg) {
+	qname := strings.ToLower(r.Question[0].Name)
+	if blockedDomains[qname] {
+		log.Printf("Blocked domain requested: %s\n", qname)
+		m := new(dns.Msg)
+		m.SetReply(r)
+		m.Rcode = dns.RcodeNameError
+		w.WriteMsg(m)
+		return
+	}
 	for _, question := range r.Question {
 		log.Printf("Received query for %s\n", question.Name)
 	}
